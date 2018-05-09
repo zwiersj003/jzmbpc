3b7bc7515bce0e7cdad29b9700bb4ad3b84d3f7b
Melanie Backers <melanie.backers@hva.nl>
2018-05-09 16:29:20 +0200

    Revert "make project"
    This reverts commit ca78ebdc72ac3851adbd7f76626daf4b7e98e69f.

--- ParallelComputing/RMIConcept/src/Service.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Service extends Remote {
-    String sendMessage(String message) throws RemoteException;
-}
--- ParallelComputing/RMIGenericTaskExchange/src/Task.java
+++
@@ -1,3 +0,0 @@
-public interface Task<T> {
-    T execute();
-}
